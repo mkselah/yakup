ec5c20160c92ad2d624afc81900fed7d5eaa60de
Mohammad Salahadeen <salahadeenm@gmail.com>
2026-09-12 21:56:54 +0200

    new sheet

--- netlify/functions/sheet.js
+++ netlify/functions/sheet.js
@@ -1,6 +1,6 @@
 export async function handler(event, context) {
   // REPLACE with your sheet ID:
-  const SHEET_ID = '10nEc8I8XssKCdwbP2A3MOI5TwZQ49Qzg1yGkFDKQkt8';
+  const SHEET_ID = '1U2ZmiGJfotFVYGRGytANJosiWQJHJcMSSoMzP4DUU4Q';
   const CSV_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv`;
 
    
